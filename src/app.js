540dcf7af1274fbf79f2d55d7390d22a253d9cfc
khdigital94 <153145008+khdigital94@users.noreply.github.com>
2024-08-13 00:35:46 +0200

    Added basic filter functionality for To-Do status and function to add new To-Dos to the list

--- src/app.js
+++ src/app.js
@@ -1,25 +1,43 @@
-const ToDoContainer = document.querySelector(".task-container");
+const toDoContainer = document.querySelector(".task-container");
+const toDoFilter = document.getElementsByClassName("to-do-filter");
+
+const popup = document.querySelector(".popup");
+const popupOverlay = document.querySelector(".popup-overlay");
+const popupButton = document.querySelector(".popup-button");
+const newTaskButton = document.querySelector(".newtask-button");
 
 let toDos = [
   {
     name: "Aufräumen",
-    category: "Personal",
-    status: "offen",
+    category: "Persönlich",
+    status: "Offen",
     dateCreated: "",
   },
   {
     name: "Programmieren lernen",
-    category: "Personal",
-    status: "offen",
+    category: "Persönlich",
+    status: "Offen",
+    dateCreated: "",
+  },
+  {
+    name: "Abendessen kochen",
+    category: "Persönlich",
+    status: "Abgeschlossen",
+    dateCreated: "",
+  },
+  {
+    name: "Neue Kampagne erstellen",
+    category: "Arbeit",
+    status: "Abgeschlossen",
     dateCreated: "",
   },
 ];
 
-function showToDos() {
-  ToDoContainer.innerHTML = "";
+function showToDos(toDos) {
+  toDoContainer.innerHTML = "";
 
   toDos.forEach((task) => {
-    ToDoContainer.innerHTML += `
+    toDoContainer.innerHTML += `
         <div class="single-task bg-slate-100 rounded p-2 flex gap-4">
             <div class="task-left flex content-center align-center">
                 <input type="checkbox" />
@@ -33,4 +51,58 @@ function showToDos() {
   });
 }
 
-showToDos();
+// To Do Filter
+let filteredToDos = [];
+
+for (let filter of toDoFilter) {
+  let currentFilter = filter.textContent;
+
+  filter.addEventListener("click", () => {
+    filteredToDos = [];
+
+    if (currentFilter === "Alle") {
+      showToDos(toDos);
+    } else {
+      toDos.forEach((task) => {
+        if (task.status === currentFilter) {
+          filteredToDos.push(task);
+        }
+
+        showToDos(filteredToDos);
+      });
+    }
+  });
+}
+
+// Popup Functionality
+popupButton.addEventListener("click", () => {
+  popup.style.display = "block";
+});
+
+popupOverlay.addEventListener("click", () => {
+  popup.style.display = "none";
+});
+
+function addNewTask() {
+  const newTaskName = document.querySelector(".popup input");
+  const newTaskCategory = document.querySelector(".popup select");
+  let newTask = {
+    name: newTaskName.value,
+    category: newTaskCategory.value,
+  };
+
+  newTaskName.value = "";
+  newTaskCategory.value = "Persönlich";
+
+  popup.style.display = "none";
+
+  toDos.push(newTask);
+  showToDos(toDos);
+}
+
+newTaskButton.addEventListener("click", () => {
+  event.preventDefault();
+  addNewTask();
+});
+
+showToDos(toDos);
